0abc6e59c9c630fa496dedba6aefc2924a411af9
TynishaN <tniles@statisticsguyana.gov.gy>
2025-04-01 16:30:00 -0400

    Update cachematrix.R
    This R script optimizes the matrix inversion by caching previously computed results, reducing redundant calculations.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+inv <- NULL   #initial cache for the inverse
+        
+        #function to set the matrix
+        set <- function(y) {
+                x <<- y     #store the new matrix
+                inv <- NULL  #reset cached inverse (since the matrix changed)
+                }
+        
+        get <- function() x   #function to get the matrix
+        setInverse <- function(inverse) inv <<- inverse #function to set the inverse (cache the results)
+        getInverse <- function() inv  #function to get the cached inverse
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse() #check if inverse is already cached
+        if(!is.null(inv)){
+                message("Getting cached inverse")
+                return(inv)
 }
+       mat <- x$get()   #get the matrix
+       inv <- solve(mat, ...) #compute the inverse using solve()
+           x$setInverse(inv) # store the computed inverse in cache
+           inv #return the inverse
+           }
